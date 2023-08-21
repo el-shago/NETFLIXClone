d797f0b13630170e18fed24dd873493c7422e8db
el-shago <92878520+el-shago@users.noreply.github.com>
2023-08-20 18:00:19 -0700

    Deployment

--- netflix-clone/components/Billboard.tsx
+++ netflix-clone/components/Billboard.tsx
@@ -1,27 +1,34 @@
-import React from 'react';
-import useBillboard from '@/hooks/useBillboard';
-import { AiOutlineInfoCircle } from 'react-icons/ai';
-import PlayButton from './PlayButton';
+import React, { useCallback } from "react";
+import useBillboard from "@/hooks/useBillboard";
+import { AiOutlineInfoCircle } from "react-icons/ai";
+import PlayButton from "./PlayButton";
+import useInfoModal from "@/hooks/useinfoModal";
 
 const Billboard = () => {
-    const { data } = useBillboard();
-    return(
-        <div className='relative h-[56.85vw]'>
-            <video
-            className='
+  const { data } = useBillboard();
+  const { openModal } = useInfoModal();
+
+  const handleOpenModal = useCallback(() => {
+    openModal(data?.id);
+  }, [openModal, data?.id]);
+  return (
+    <div className="relative h-[56.85vw]">
+      <video
+        className="
             w-full
             h-[56.25vw]
             object-cover
             brightness-[60%]
-            '
-            autoPlay
-            muted
-            loop 
-            poster={data?.thumbnailUrl}
-            src={data?.videoUrl}>
-            </video>
-            <div className='absolute top-[30%] md:top-[40%] ml-4 md:ml-16]'>
-                <p className='
+            "
+        autoPlay
+        muted
+        loop
+        poster={data?.thumbnailUrl}
+        src={data?.videoUrl}
+      ></video>
+      <div className="absolute top-[30%] md:top-[40%] ml-4 md:ml-16]">
+        <p
+          className="
                 text-white 
                 text-1xl 
                 md:text-5xl 
@@ -30,10 +37,12 @@ const Billboard = () => {
                 lg:text-6xl 
                 font-bold 
                 drop-shadow-xl
-                '>
-                    {data?.title}
-                </p>
-                <p className='
+                "
+        >
+          {data?.title}
+        </p>
+        <p
+          className="
                 text-white
                 text-[8px]
                 md:text-lg
@@ -43,13 +52,15 @@ const Billboard = () => {
                 md:w-[80%]
                 lg:w-[50%]
                 drop-shadow-xl
-                '>
-                    {data?.description}
-                </p>
-                <div className='flex flex-row items-center mt-3 md:mt-4 gap-3'>
-                <PlayButton movieId={data?.id} />
-                <button 
-                className='
+                "
+        >
+          {data?.description}
+        </p>
+        <div className="flex flex-row items-center mt-3 md:mt-4 gap-3">
+          <PlayButton movieId={data?.id} />
+          <button
+            onClick={handleOpenModal}
+            className="
                     bg-white
                     text-white
                     bg-opacity-30
@@ -64,14 +75,15 @@ const Billboard = () => {
                     items-center
                     hover:bg-opacity-20
                     transition
-                '>
-                <AiOutlineInfoCircle className='mr-1'/>
-                    More Info
-                </button>
-                </div>
-            </div>
+                "
+          >
+            <AiOutlineInfoCircle className="mr-1" />
+            More Info
+          </button>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Billboard;
--- netflix-clone/pages/index.tsx
+++ netflix-clone/pages/index.tsx
@@ -5,7 +5,8 @@ import Billboard from '@/components/Billboard';
 import useMovieList from '@/hooks/useMovieList';
 import MovieList from '@/components/MovieList';
 import useFavorites from '@/hooks/useFavorites';
-
+import InfoModal from '@/components/InfoModal';
+import useInfoModal from '@/hooks/useinfoModal';
 
 export async function getServerSideProps(context: NextPageContext){
   const session = await getSession(context);
@@ -25,10 +26,12 @@ export async function getServerSideProps(context: NextPageContext){
 }
 
 export default function Home() {
-  const{ data: movies = []} = useMovieList();
-  const{ data: favorites = []} = useFavorites();
+  const { data: movies = []} = useMovieList();
+  const { data: favorites = []} = useFavorites();
+  const {isOpen, closeModal } = useInfoModal();
   return (
     <>
+    <InfoModal visible={isOpen} onClose={closeModal} />
      <Navbar/>
      <Billboard/>
      <div className='pb-40'>
